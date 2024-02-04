5bd27ee928b320c354aaff5aa6cc62e22cefd7b5
Onur Akyüz <49249821+OnurAkyuz61@users.noreply.github.com>
2024-02-04 19:54:18 +0300

    Add files via upload

---
+++ Dairenin Alanını ve Çevresini Hesaplayan Program/daire.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    // Dairenin yarıçapını ve hesaplanan alan, çevre değerlerini saklamak için değişkenleri tanımla
+    double yaricap, alan, cevre;
+    // Sabit pi sayısını tanımla
+    const double pi = 3.14;
+
+    // Kullanıcıdan yarıçapı al
+    printf("Dairenin Yaricapini Yazin:");
+    scanf("%lf", &yaricap);
+
+    // Dairenin alanını ve çevresini hesapla
+    alan = pi * yaricap * yaricap;
+    cevre = 2 * pi * yaricap;
+
+    // Hesaplanan değerleri ekrana yazdır
+    printf("Dairenin cevresi %.1lf\n", cevre);
+    printf("Dairenin alani %.1lf\n", alan);
+
+    return 0;
+}
