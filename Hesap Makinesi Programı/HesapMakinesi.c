1683a2b014cc404d2563d5ba32babbb94117fffb
Onur Akyüz <49249821+OnurAkyuz61@users.noreply.github.com>
2024-02-05 23:03:31 +0300

    Add files via upload

---
+++ Hesap Makinesi Programı/HesapMakinesi.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+	double sayi1, sayi2, cevap;
+	char islem;
+
+	printf("Ilk	Sayiyi Girin:");
+	scanf("%lf", &sayi1);
+
+	printf("Ikinci Sayiyi Girin:\n");
+	scanf("%lf", &sayi2);
+
+	printf("Yapacaginiz islemi secin (+ - * /):\n");
+	scanf("%c", &islem);
+
+	switch (islem)
+	{
+	case '+':
+		cevap = sayi1 + sayi2;
+		printf("Cevap = &lf\n", cevap);
+		break;
+	case '-':
+		cevap = sayi1 - sayi2;
+		printf("Cevap = &lf\n", cevap);
+		break;
+	case '*':
+		cevap = sayi1 * sayi2;
+		printf("Cevap = &lf\n", cevap);
+		break;
+	case '/':
+		cevap = sayi1 / sayi2;
+		printf("Cevap = &lf\n", cevap);
+		break;
+	default:
+		printf("&c gecerli bir islem degil", islem);
+		break;
+	}
+
+	return 0;
+}
